0425ad32fea915c8afafb76590786871ad6b48cb
Stpar <suntipap1312@gmail.com>
2021-08-05 16:18:50 +0700

    Create Exercise4_Suntipap_Aree.py

---
+++ Exercise4_Suntipap_Aree.py
@@ -0,0 +1,10 @@
+FoundationEnglish = FE = 70
+GeneralBusiness = GB = 68.7
+IntroductionToComputerSystems = ICS = 79.4
+ComputerProgramming = CP = 60.5
+
+print("---Your Score---")
+print("Foundation English :",FE )
+print("General Business :",GB )
+print("Introduction To Computer Systems :",ICS )
+print("Computer Programming :",CP )
